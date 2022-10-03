a48f63599f37530c8170ded0bdac61fea9b95d5c
Jon Sadowski <jwsadowski@gmail.com>
2022-10-02 22:56:14 -0500

    second commit

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-var timeBlockEl = $("timeblock");
+var timeBlockEl = $(".timeblock");
+var timeDisplayEl = $("#currentDay");
+var saveBtn = $("save-button");
 
 var today = moment();
 $(".jumbotron").text(today.format("MMM Do, YYYY"));
 
-var currentTime = moment().format("HH:mm");
+timeDisplayEl.text(day);
 
-function colorCode() {
-    if (timeBlockEl < currentTime) {
-        
-    }
-}
+var currentTime = moment().format("HH a");
+
+function pastPresentFuture() {
+    if (timeBlockEl < currentTime)
+    console.log("Event that already happened");
+} else if (timeBlockEl === currentTime)
+    console.log("Current hour");
+
+
+timeBlockEl.addEventListener('click', function(event) {
+    timeBlockEl.textContent();
+
+    
+
+    localStorage.setItem("input", JSON.stringify(input));
+});
+
+saveBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+})
+
+
+
+
 
 
     
